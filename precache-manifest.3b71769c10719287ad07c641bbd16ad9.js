259948e9979f244cf655c1fed911a397793b741f
Rm Hridoy <55066846+rmhridoy@users.noreply.github.com>
2020-08-21 12:05:08 +0600

    Updates

---
+++ precache-manifest.3b71769c10719287ad07c641bbd16ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5e64bd0038f6460ff9f9b69b64c1cd",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "3e3ae2f771949ab2d26d",
+    "url": "/messenger-clone/static/css/main.2e5147c8.chunk.css"
+  },
+  {
+    "revision": "e29d9f82f7be5caced62",
+    "url": "/messenger-clone/static/js/2.aeb2070e.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/messenger-clone/static/js/2.aeb2070e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3ae2f771949ab2d26d",
+    "url": "/messenger-clone/static/js/main.d5a20975.chunk.js"
+  },
+  {
+    "revision": "82dbdf3212f711bb7f96",
+    "url": "/messenger-clone/static/js/runtime-main.0297d229.js"
+  }
+]);
